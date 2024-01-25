51ee5cd7f89d15f94e4c7551ba3d9e3d0b48d508
Luke Goodfellow <goodfellow.luke@icloud.com>
2024-01-25 18:41:52 +0000

    Fix error from saveHelmReleases

--- src/main.js
+++ src/main.js
@@ -55,7 +55,7 @@ async function saveHelmReleases(type, namespace) {
   try {
     const helmList = new Deno.Command('helm', { args: ['list', '-n', namespace, '-o', 'json'] });
     const output = await helmList.output();
-    const helmReleases = JSON.parse(new TextDecoder().decode(output));
+    const helmReleases = JSON.parse(new TextDecoder().decode(output.stdout));
     await Deno.writeTextFile(`${dirPath}/${type}-helmReleases.yaml`, toYaml(helmReleases, { skipInvalid: true }));
   } catch (error) {
     console.error(`Error saving Helm releases for ${type}:`, error);
