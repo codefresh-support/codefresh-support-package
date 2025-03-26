82f13de8b2ea6ff431bebbc4070cd93515b37ff3
Noam Gal <atgardner@gmail.com>
2025-03-27 02:09:03 +1100

    fix: update K8s resource names to use fully-qualified names (#26)
    * fix: update K8s resource names to use fully-qualified names
    
    * converted everythign to fully qualified names. have dirs to be the same.
    
    ---------
    
    Co-authored-by: Luke Goodfellow <107487942+lukasmagik@users.noreply.github.com>

--- internal/k8s/runtime_resources.go
+++ internal/k8s/runtime_resources.go
@@ -1,41 +1,41 @@
 package k8s
 
 var K8sGeneral = []string{
-	"Configmaps",
-	"DaemonSets",
-	"Deployments",
-	"Jobs",
-	"Nodes",
-	"Pods",
-	"ServiceAccounts",
-	"Services",
-	"StatefulSets",
+	"configmaps",
+	"daemonsets.apps",
+	"deployments.apps",
+	"jobs.batch",
+	"nodes",
+	"pods",
+	"serviceaccounts",
+	"services",
+	"statefulsets.apps",
 }
 
 var K8sClassicOnPrem = []string{
-	"CronJobs",
-	"PersistentVolumeClaims",
-	"PersistentVolumes",
-	"Storageclass",
+	"cronjobs.batch",
+	"persistentvolumeclaims",
+	"persistentvolumes",
+	"storageclasses.storage.k8s.io",
 }
 
 var K8sGitOps = []string{
-	"Products",
-	"PromotionFlows",
-	"PromotionPolicies",
-	"PromotionTemplates",
-	"RestrictedGitSources",
+	"products.codefresh.io",
+	"promotionflows.codefresh.io",
+	"promotionpolicies.codefresh.io",
+	"promotiontemplates.codefresh.io",
+	"restrictedgitsources.codefresh.io",
 }
 
 var K8sArgo = []string{
-	"AnalysisRuns",
-	"AnalysisTemplates",
-	"Applications",
-	"ApplicationSets",
-	"AppProjects",
-	"EventBus",
-	"EventSources",
-	"Experiments",
-	"Rollouts",
-	"Sensors",
+	"analysisruns.argoproj.io",
+	"analysistemplates.argoproj.io",
+	"applications.argoproj.io",
+	"applicationsets.argoproj.io",
+	"appprojects.argoproj.io",
+	"eventbus.argoproj.io",
+	"eventsources.argoproj.io",
+	"experiments.argoproj.io",
+	"rollouts.argoproj.io",
+	"sensors.argoproj.io",
 }
