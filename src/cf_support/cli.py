ba5f2867aa6112d648c0893c8797d2e0d7ab5248
Luke Goodfellow <107487942+ThatAmatoGuy@users.noreply.github.com>
2025-07-17 21:10:44 +0000

    updated files

--- src/cf_support/cli.py
+++ src/cf_support/cli.py
@@ -1,7 +1,14 @@
 import click
+import importlib.metadata
 from .commands import pipelines, gitops, onprem, oss
 
+try:
+    __version__ = importlib.metadata.version("cf-support")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "0.0.0+dev"
+
 @click.group()
+@click.version_option(version=__version__, prog_name="cf-support")
 def cli():
     """Codefresh Support Package
 
