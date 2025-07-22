703f4a911a928c70bd2f54539d594ea29a3f2ba9
Luke Goodfellow <107487942+ThatAmatoGuy@users.noreply.github.com>
2025-07-22 11:52:30 -0700

    update version import

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ env_url = os.getenv("CF_URL")
 auth_controller = AuthController(env_token, env_url)
 
 try:
-    from ._version import version as __version__
+    from _version import version as __version__
 except ImportError:
     __version__ = "0.0.0+dev.uninstalled"
 
