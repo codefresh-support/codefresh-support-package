15060ba7c4255ac7e7b0dc1853f16751d6ded1c1
Luke Goodfellow <107487942+ThatAmatoGuy@users.noreply.github.com>
2025-07-22 11:50:50 -0700

    updated main.py and toml to get cli to work

--- src/main.py
+++ src/main.py
@@ -12,31 +12,30 @@ load_dotenv()
 logger = setup_logger(__name__)
 
 
-def main():
-    logger.info("Starting CF Support CLI")
-    env_token = os.getenv("CF_API_KEY")
-    env_url = os.getenv("CF_URL")
-    auth_controller = AuthController(env_token, env_url)
+logger.info("Starting CF Support CLI")
+env_token = os.getenv("CF_API_KEY")
+env_url = os.getenv("CF_URL")
+auth_controller = AuthController(env_token, env_url)
 
-    try:
-        from ._version import version as __version__
-    except ImportError:
-        __version__ = "0.0.0+dev.uninstalled"
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "0.0.0+dev.uninstalled"
 
-    @click.group()
-    @click.version_option(version=__version__, prog_name="cf-support")
-    def cli():
-        """Codefresh Support Package
+@click.group()
+@click.version_option(version=__version__, prog_name="cf-support")
+def cli():
+    """Codefresh Support Package
 
-        Tool to gather information for Codefresh Support
-        """
-        pass
+    Tool to gather information for Codefresh Support
+    """
+    pass
 
-    cli.add_command(pipelines.pipelines_command)
-    cli.add_command(gitops.gitops_command)
-    cli.add_command(onprem.onprem_command)
-    cli.add_command(oss.oss_command)
+cli.add_command(pipelines.pipelines_command)
+cli.add_command(gitops.gitops_command)
+cli.add_command(onprem.onprem_command)
+cli.add_command(oss.oss_command)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
